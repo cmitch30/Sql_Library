2ece9744a5b99e1307c60196b0f8bc08fa91bad2
clark <clark_mitchelljr@hotmail.com>
2023-01-02 13:49:56 -0700

    update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var indexRouter = require("./routes/index");
-const sequelize = require("./models/index").sequelize;
+const sequelize = require("./models/index").sequelize
 const bookRouter = require('./routes/books')
 
 
